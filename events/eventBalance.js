1f4a6a7612895cc62a93950d1673bbad9af6eb38
ALWrite <159183530+ALWrite@users.noreply.github.com>
2024-08-19 18:42:14 +0700

    add cooldown

--- events/eventBalance.js
+++ events/eventBalance.js
@@ -1,49 +1,87 @@
-const { Events, ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
-const { thumbnailURL, imageURL, wlEmoji, emoji1, emoji2, StoreName } = require('../config.json');
+const { Events, EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js');
+const { emoji1, emoji2 } = require('../config.json');
 const User = require('../models/user');
 
 module.exports = {
   name: Events.InteractionCreate,
-  async execute(interaction) {
-    if (interaction.customId === 'getBalance') {
-      const username = interaction.user.username;
-      const discordId = interaction.user.id;
-     
+  async execute(interaction, client) {
+    // Check if the interaction is a button click
+    if (interaction.isButton()) {
+      const buttonId = interaction.customId;
+      
+      // Check for the 'getBalance' button click
+      if (buttonId === 'getBalance') {
+        const { cooldowns } = client;
+        const defaultCooldownDuration = 5; // Cooldown duration in seconds
+        const cooldownAmount = defaultCooldownDuration * 1000;
+        const now = Date.now();
 
-      try {
-        
+        if (!cooldowns.has(buttonId)) {
+          cooldowns.set(buttonId, new Collection());
+        }
 
-        const user = await User.findOne({ discordId });
+        const timestamps = cooldowns.get(buttonId);
 
-      if (!user) {
-        const belum = new EmbedBuilder()
-        .setTitle('Warning!!')
-        .setDescription('Use this button to set your grow Id');
-        const button = new ButtonBuilder()
-        .setCustomId('growid')
-    	.setLabel('Set GrowId')
-	    .setStyle(ButtonStyle.Danger)
-        .setEmoji('1219156061727101040');
-        
-      const row = new ActionRowBuilder()
-      .addComponents(button)
-     return interaction.reply({ embeds: [belum], components: [row], ephemeral: true });
-      }
+        if (timestamps.has(interaction.user.id)) {
+          const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
 
-      // Send the user's GrowID and formatted balance information
-      const depoEmbed = new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle('Information GrowId')
-        .setDescription(`${emoji2}  Name: **${user.growId}**
-${emoji1}  Balance: **${user.balance.toLocaleString('id-ID')}**`)
+          if (now < expirationTime) {
+            const expiredTimestamp = Math.round(expirationTime / 1000);
+            return interaction.reply({
+              content: `Please wait, you are on a cooldown for this button. You can use it again <t:${expiredTimestamp}:R>.`,
+              ephemeral: true
+            });
+          }
+        }
 
-      return interaction.reply({ embeds: [depoEmbed], ephemeral: true }); 
-      } catch (error) {
-        console.error('Error:', error);
-        return interaction.reply({ content: 'Something went wrong.', ephemeral: true });
+        timestamps.set(interaction.user.id, now);
+        setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+        // Proceed with handling the interaction
+        const username = interaction.user.username;
+        const discordId = interaction.user.id;
+
+        try {
+          const user = await User.findOne({ discordId });
+
+          if (!user) {
+            const belum = new EmbedBuilder()
+              .setTitle('Warning!!')
+              .setDescription('Use this button to set your GrowId');
+            
+            const button = new ButtonBuilder()
+              .setCustomId('growid')
+              .setLabel('Set GrowId')
+              .setStyle(ButtonStyle.Danger)
+              .setEmoji('1219156061727101040');
+            
+            const row = new ActionRowBuilder().addComponents(button);
+
+            return interaction.reply({
+              embeds: [belum],
+              components: [row],
+              ephemeral: true
+            });
+          }
+
+          const depoEmbed = new EmbedBuilder()
+            .setColor('#0099ff')
+            .setTitle('Information GrowId')
+            .setDescription(`${emoji2} Name: **${user.growId}**
+${emoji1} Balance: **${user.balance.toLocaleString('id-ID')}**`);
+
+          return interaction.reply({
+            embeds: [depoEmbed],
+            ephemeral: true
+          });
+        } catch (error) {
+          console.error('Error:', error);
+          return interaction.reply({
+            content: 'Something went wrong.',
+            ephemeral: true
+          });
+        }
       }
     }
   }
 };
-
-        
